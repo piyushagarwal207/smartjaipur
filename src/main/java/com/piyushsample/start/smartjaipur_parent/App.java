a83c048f785f084951b3854701325351094dbecf
Piyush Agarwal <piyushagarwal207@outlook.com>
2018-09-30 02:33:45 +0530

    My First Commit for smartjaipur

---
+++ src/main/java/com/piyushsample/start/smartjaipur_parent/App.java
@@ -0,0 +1,13 @@
+package com.piyushsample.start.smartjaipur_parent;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
